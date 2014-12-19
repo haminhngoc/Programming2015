fae5ebd1225b0ec1cafd2430cd6e6a7dcf3a1f8f
haminhngoc <haminhngoc@yahoo.com>
2014-12-19 23:07:50 +0700

    Top of Java as usual
    http://vn.spoj.com/ranks/BONUS/lang=JAVA

---
+++ Private/Ngoc/Spoj/8164-VOI 2011-Phan-Thuong/BONUS.java
@@ -0,0 +1,180 @@
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+class BONUS {
+	static InputStream is;
+	static PrintWriter out;
+	static String INPUT = "";
+
+	public static void main(String[] args) throws Exception {
+		oj = true;
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis() - s + "ms");
+	}
+
+	static void solve() {
+		int n = ni();
+		int k = ni();
+
+		int[][] matrix = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			int[] preRow = matrix[i - 1];
+			int top = 0, leftTop, left = 0;
+			for (int j = 1; j <= n; j++) {
+				int value = ni();
+				leftTop = top;
+				top = preRow[j];
+				left = left + top - leftTop + value;
+				matrix[i][j] = left;
+			}
+		}
+
+		int maxValue = 0;
+		for (int i = 0; i <= n - k; i++) {
+			for (int j = 0; j <= n - k; j++) {
+				int x = i + k;
+				int y = j + k;
+				int value = matrix[x][y] - matrix[x][j] - matrix[i][y] + matrix[i][j];
+				if (value > maxValue) {
+					maxValue = value;
+				}
+			}
+		}
+		out.println(maxValue);
+	}
+
+	/*
+	 * *****************************************************************
+	 * ******************* BASIC READER ******************************* ***************************************************************
+	 */
+
+	static byte[] inbuf = new byte[16384];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static char[][] nm(int n, int m) {
+		char[][] map = new char[n][];
+		for (int i = 0; i < n; i++)
+			map[i] = ns(m);
+		return map;
+	}
+
+	static int[] na(int n) {
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = ni();
+		return a;
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+
+	static void tr(Object... o) {
+		if (!oj) {
+			System.out.println(Arrays.deepToString(o));
+		}
+	}
+}
